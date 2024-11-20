a0b90b0753e1031bafd0a9431601463269a4d399
albertob1412 <abaranqf@gmail.com>
2024-11-20 03:57:54 +0100

    otra mas

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-// Configuración de Solana
-const solanaWeb3 = require('@solana/web3.js');
-const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'));
-
-// Dirección del token
-const tokenAddress = new solanaWeb3.PublicKey('Df6yfrKC8kZE3KNkrHERKzAetSxbrWeniQfyJY4Jpump');
-
-// Función para cambiar el color de fondo
 function changeBackgroundColor() {
   const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   document.body.style.backgroundColor = randomColor;
