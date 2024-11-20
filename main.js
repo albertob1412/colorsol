25f7a69b6cc97f5edb790744864838b9252e5580
albertob1412 <abaranqf@gmail.com>
2024-11-20 03:40:04 +0100

    hazlo

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const solanaWeb3 = require('@solana/web3.js');
 const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'));
+
+// Dirección del token que quieres monitorear
 const tokenAddress = new solanaWeb3.PublicKey('Df6yfrKC8kZE3KNkrHERKzAetSxbrWeniQfyJY4Jpump');
 
+// Función que cambia el color del fondo
 function changeBackgroundColor() {
-    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    document.body.style.backgroundColor = randomColor;
-    console.log("Color cambiado a:", randomColor);
-  }
-  
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  document.body.style.backgroundColor = randomColor;
+  console.log("Color cambiado a:", randomColor);
+}
 
+// Escucha eventos en la cuenta del token
 connection.onLogs(tokenAddress, (logs, context) => {
+  console.log("Evento detectado:", logs);
   if (logs.err === null) {
     changeBackgroundColor();
   }
