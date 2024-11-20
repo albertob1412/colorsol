4a8b30cd2840a605037433b58464991040eafe77
albertob1412 <abaranqf@gmail.com>
2024-11-20 03:34:19 +0100

    colores

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-b
 const tokenAddress = new solanaWeb3.PublicKey('Df6yfrKC8kZE3KNkrHERKzAetSxbrWeniQfyJY4Jpump');
 
 function changeBackgroundColor() {
-  const colors = ['#FF5733', '#33FF57', '#3357FF', '#F333FF'];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.backgroundColor = randomColor;
-}
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    document.body.style.backgroundColor = randomColor;
+    console.log("Color cambiado a:", randomColor);
+  }
+  
 
 connection.onLogs(tokenAddress, (logs, context) => {
   if (logs.err === null) {
